9304f40fdb20dc3d98f8d6ae16a84c99f20644a2
Larisett <larissa.monteiro.oliveira@gmail.com>
2024-09-25 10:41:09 -0300

    Criei o repositorio

---
+++ atividade.js
@@ -0,0 +1,68 @@
+1
+nome = "Larissa" 
+console.log(nome)
+idade = 18 
+console.log(idade)
+mensagem = "ola sou larissa e tenho 18 anos" 
+console.log(mensagem)
+
+2
+nome = "Larissa"
+cidade = "Recife"
+variavel = (nome + " é de " + cidade)
+console.log(variavel)
+
+3
+const num1=7
+const num2=10
+
+const soma= (num1 + num2)
+console.log(soma)
+
+const menos= (num1 - num2)
+console.log(menos)
+
+const mutiplicação= (num1 * num2)
+console.log(mutiplicação)
+
+const dividir= (num1 / num2)
+console.log(dividir)
+
+4
+base = 6
+altura = 18
+area = (base * altura / 2)
+console.log(area)
+
+5
+nota1 = (9)
+nota2 = (4)
+nota3 = (5)
+
+media =(nota1 + nota2 + nota3) / 3
+console.log("a media é " + media)
+
+6
+preço = 500
+desconto = 25
+calculo =(500 * 0.25)
+calculo=(500 - 125) 
+console.log(preço)
+console.log("O desconto é de 25% e de 125 reais")
+
+7
+
+8
+
+9
+celsius = 35
+fahrenheit= (35 * 9/5) + 32
+console.log("O clima está a " + fahrenheit + " fahrenheit")
+
+10
+peso= 70
+altura= 1.80
+IMC= peso/(altura * altura)
+console.log(IMC)
+
+Não sei o 7 e o 8:(
